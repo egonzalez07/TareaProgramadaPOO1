04f30227d2cf1665649f68ac921930176ad3cd62
elka gonzalez <elkgm07@gmail.com>
2015-03-29 19:15:38 -0600

    ParqueoCarros

--- TareaProgramadaPOO1/src/Grafica/InicioParqueo.java
+++ TareaProgramadaPOO1/src/Grafica/InicioParqueo.java
@@ -302,9 +302,5 @@ public class InicioParqueo extends javax.swing.JFrame{
 //
 //    }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
 }

b9c2418a50fbac5ec3e6e9625215f7893a520ec0
MX32 <marvindurann@gmail.com>
2015-03-31 17:02:06 -0600

    Lista Pila

---
+++ TareaProgramadaPOO1/src/Clases/Caja.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Marvin
+ */
+public class Caja {
+    int dinero;
+    public Caja(){}
+    
+    public int getDinero(){
+        return this.dinero;
+    }
+    public void setDinero(int dinero){
+        this.dinero += dinero;
+    }
+    public void sacarDinero(int dinero){
+        
+        if(this.dinero>dinero){this.dinero -= dinero;}   
+        JOptionPane.showMessageDialog(null,"No hay dinero");
+    }   
+}

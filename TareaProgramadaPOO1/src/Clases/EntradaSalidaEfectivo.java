ae22f979efe3c51eecf5a82abd1260ac0c6714bb
MX32 <marvindurann@gmail.com>
2015-03-31 17:06:09 -0600

    EntradaSalidaEfectivos

---
+++ TareaProgramadaPOO1/src/Clases/EntradaSalidaEfectivo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Marvin
+ */
+
+public class EntradaSalidaEfectivo {
+    
+    Caja caja;
+    String contraseñaAdministrador;
+    public EntradaSalidaEfectivo(){}
+    
+    public void ingresarDinero(int monto,String contraseña){
+        if(contraseña.equals(contraseñaAdministrador)){ caja.setDinero(monto);} 
+        JOptionPane.showMessageDialog(null,"Las contraseñas no coinciden");
+    }
+    public void retirarDinero(int monto, String contraseña){
+        if(contraseña.equals(contraseñaAdministrador)){
+           
+            if (caja.getDinero()>=monto){caja.sacarDinero(monto);}
+            JOptionPane.showMessageDialog(null,"Fondos insuficientes");
+            
+        }JOptionPane.showMessageDialog(null,"Las contraseñas no coinciden");
+    }
+}

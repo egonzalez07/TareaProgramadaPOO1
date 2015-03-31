4c2da9fa9c9edbbe7f78a74a2873e2dee8828774
limon <limon@gollo>
2015-03-30 22:01:47 -0700

    Clase Carro

---
+++ TareaProgramadaPOO1/src/Clases/MainXML.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author alralaos
+ */
+public class MainXML {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        XML prueba=new XML();
+        prueba.escribirXML("LADUGO", "3-101-536894", "00000");
+    }
+    
+}
